790305ee9b6380a4b3565d5aa833106a6c6a4267
edesbois77 <edesbois@yahoo.co.uk>
2025-06-05 23:36:52 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
-python -m http.server 8080 --directory . &
-python tottenham_scanner.py
+echo "Starting web server..."
+python -c "
+import http.server
+import socketserver
+import threading
+import time
+
+def serve():
+    PORT = 8080
+    Handler = http.server.SimpleHTTPRequestHandler
+    with socketserver.TCPServer(('', PORT), Handler) as httpd:
+        print(f'Web server on port {PORT}')
+        httpd.serve_forever()
+
+# Start web server
+web_thread = threading.Thread(target=serve)
+web_thread.start()
+
+print('Web server started, waiting 3 seconds...')
+time.sleep(3)
+
+# Start scanner
+exec(open('tottenham_scanner.py').read())
+"
